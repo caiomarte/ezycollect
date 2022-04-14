ff82645d97518f38fe2fca21bcdccd01f7816e1e
Caio Martinho <caio-silva@kibsi.com>
2022-04-14 00:26:49 -0300

    fix

--- nginx/release.tf
+++ nginx/release.tf
@@ -1,26 +1,17 @@
 locals {
   values = {
-    name      = var.application.image
-    image     = var.application.image
-    version   = var.application.version
-    namespace = "${var.application.image}-namespace"
-    replicas  = var.application.replicas
-    port      = var.application.port
-    address   = var.cluster_ip
-    domain    = var.application.domain
     sqs = var.sqs_endpoint
     s3 = var.s3_endpoint
     manager   = "terraform"
-    author    = "caio-martinho"
   }
 }
 
 
 resource "helm_release" "nginx" {
-  name             = local.values["name"]
-  description      = "Helm Chart release for the ${local.values["image"]}:${local.values["version"]} image."
+  name             = "nginx"
+  description      = "Helm Chart release for the nginx:1.21.6-alpine image."
   chart            = "./nginx/helm"
-  namespace        = local.values["namespace"]
+  namespace        = "nginx-namespace"
   create_namespace = true
   reset_values     = true
   cleanup_on_fail  = true
